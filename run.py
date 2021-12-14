d7158e7d8efe9f56614b0c398b2e7dcac8296f94
Enrique De Nobrega <edenobrega@sky.com>
2021-12-14 19:12:24 +0000

    Implement print_banner

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ")
+print(" __          __        _   _   ")
+print(" \ \        / /       | | | |  ")
+print("  \ \  /\  / /__  __ _| |_| |__   ___ _ __   _ __  _   _ ")
+print("   \ \/  \/ / _ \/ _` | __| '_ \ / _ \ '__| | '_ \| | | |")
+print("    \  /\  /  __/ (_| | |_| | | |  __/ |    | |_) | |_| |")
+print("     \/  \/ \___|\__,_|\__|_| |_|\___|_|    | .__/ \__, |")
+print("                                            | |     __/ |")
+print("                                            |_|    |___/ \n")
+print("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ")
