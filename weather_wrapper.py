aa2046d61aa9ceb6d3875a11ff012d37e61b7481
Enrique De Nobrega <edenobrega@sky.com>
2021-12-14 19:19:37 +0000

    Implement get_json_response

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -1,3 +1,4 @@
+import requests
 """ Module for the weather api wrapper """
 
 
@@ -5,5 +6,15 @@ class Weather:
     """ Class for handling requests to the https://openweathermap.org/ api """
     def __init__(self, api_key):
         self.api_key = api_key
+
+    def json_respones(self, url):
+        response = requests.get(url)
+        if(response.status_code == requests.codes.ok):
+            return response.json()
+            
+        print("Whoops, something went wrong")
+
+    def get_current_weather(self):
+        pass
 
 
