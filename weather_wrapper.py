05dc66cd2963398b2929b7845d442b9ad8a667ee
Enrique De Nobrega <edenobrega@sky.com>
2021-12-15 02:11:15 +0000

    Implement get_current_weather, Fix get_json_response bugs

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -1,20 +1,19 @@
-import requests
 """ Module for the weather api wrapper """
+import requests
 
 
 class Weather:
     """ Class for handling requests to the https://openweathermap.org/ api """
+
+    base_url = "http://api.openweathermap.org/data/2.5/"
+
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def json_respones(self, url):
-        response = requests.get(url)
-        if(response.status_code == requests.codes.ok):
+    def get_json_response(self, url):
+        response = requests.get(url + f'&APPID={self.api_key}')
+        if response.status_code == 200:
             return response.json()
-            
-        print("Whoops, something went wrong")
 
-    def get_current_weather(self):
-        pass
-
-
+        print("Whoops, something went wrong " + str(response.status_code))
+        print(url + f'&APPID={self.api_key}')
