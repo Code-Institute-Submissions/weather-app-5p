3bc15098994574ddb589497e95df109bc33ec722
Enrique De Nobrega <edenobrega@sky.com>
2021-12-15 17:20:43 +0000

    Implement get_single_forecast

--- weather_wrapper.py
+++ weather_wrapper.py
@@ -3,6 +3,7 @@ import requests
 from datetime import datetime, timedelta
 import time
 
+
 class Weather:
     """ Class for handling requests to the https://openweathermap.org/ api """
 
@@ -28,7 +29,8 @@ class Weather:
         return self.get_json_response(request_url)
 
     def get_historical_weather(self, country, town, date):
-        """ note that without subscription cannot go futher back than 5days """
+        """ note that with free api cannot go further back than 5days """
+
         timetuple = datetime.strptime(date, "%d/%m/%Y").timetuple()
         timestamp = time.mktime(timetuple)
 
@@ -49,3 +51,38 @@ class Weather:
         )
 
         return self.get_json_response(request_url)
+
+    def get_single_forecast(self, country, town, date):
+        """ note that without subscription cannot go further than 5days """
+
+        # given date in unix time
+        param_date = datetime.strptime(date, "%d/%m/%Y")
+        # today in unix time
+        today_timestamp = int(datetime.timestamp(datetime.now()))
+        # 5 day cutoff for free api use, 432000 = 5days in seconds
+        cutoff_timestamp = today_timestamp + 432000
+
+        if int(param_date.timestamp()) not in range(today_timestamp, cutoff_timestamp):
+            print("Date surpasses 5 day limit")
+            return
+
+        request_url = (
+            self.base_url +
+            'forecast'
+            f'?q={town},{country}&mode=json'
+            '&units=metric'
+        )
+        values = self.get_json_response(request_url)
+        return_values = []
+        date_format = "%Y-%m-%d %H:%M:%S"
+
+        for i in values["list"]:
+            if (datetime.strptime(i["dt_txt"], date_format).timetuple().tm_mday
+                    == param_date.timetuple().tm_mday):
+                return_values.append(i)
+
+        return return_values
+
+# from weather_wrapper import *
+# x = Weather("488f542cdab9f254e7b00827c439d379")
+# y = x.get_single_forecast("united kingdom","crawley","16/12/2021")
