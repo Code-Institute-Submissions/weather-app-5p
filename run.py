1e6b0c97e4440fcdc73871043e08e6c76fd1aa3c
Enrique De Nobrega <edenobrega@sky.com>
2022-01-04 19:01:49 +0000

    Implement Word Completer, move input to function

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 import weather_wrapper
 import os
+import pycountry
 from datetime import datetime
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+if os.path.exists('env.py'):
+    import env  # noqa
 
 message = ""
 data_message = ""
+country_tuples = []
 
+for i in pycountry.countries:
+    country_tuples.append((i.name, i.alpha_2))
+
+
+country_list = [y[0] for y in country_tuples]
 
 def print_banner():
     print("☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ☁ ")
@@ -22,17 +33,57 @@ def print_banner():
 def print_menu():
     print(message)
     print("1) Get Todays Weather")
-    print("2) Number Two")
+    print("2) See the forecast for next 5 days")
     print("3) Number Three")
     print("4) Number Four")
     print(data_message)
 
 
-weather = weather_wrapper.Weather("ff1832382941d09cdaaa8c1882e88ade")
-print_banner()
-print_menu()
+def get_selection_country():
+    country_completer = WordCompleter(country_list)
+    while True:
+        os.system("clear")
+        print_banner()
+        print("Note: Auto completer is case sensitive")
+        text = prompt("Enter a country: ", completer=country_completer)
+        if text in country_list:
+            return text
+        print("Please select a country using the auto completer")
+
+
+def get_todays_weather():
+    country = get_selection_country()
+    town = input("> Enter Town Name : ")
+    print("> Press enter to continue!")
+    data = weather.get_current_weather(country, town)
+    
+    sunrise = datetime.utcfromtimestamp(int(data['sys']['sunrise'])).strftime('%H:%M:%S')
+    sunset = datetime.utcfromtimestamp(int(data['sys']['sunset'])).strftime('%H:%M:%S')
+
+    return (
+        f"\nAt a temperature of {data['main']['temp']}c with lows of {round(data['main']['temp_min'])}c and highs of {round(data['main']['temp_max'])}c\n"
+        f"With {data['main']['humidity']}% humidity and {data['clouds']['all']}% cloud coverage\n"
+        f"Sunrise at {sunrise} and Sunset at {sunset}"
+        )
+
+
+def get_forecast():
+    country = get_selection_country()
+    town = input("> Enter Town Name : ")
+    print("> Press enter to continue!")
+    data = weather.get_full_forecast(country, town)
+    text = ""
+    for forecast in data:
+        text += f"{forecast['dt_txt']} {forecast['main']['Temp']}\n"
+    return text
+
+
+weather = weather_wrapper.Weather(os.environ.get("API_KEY"))
 
 while True:
+    os.system("clear")
+    print_banner()
+    print_menu()    
     selection = input("> ")
     if not selection.isdigit():
         os.system("clear")
@@ -56,22 +107,13 @@ while True:
     selection = int(selection)
 
     if selection == 1:
-        country = input("> Enter Country Name : ")
-        town = input("> Enter Town Name : ")
-        print("> Press enter to continue!")
-        data = weather.get_current_weather(country, town)
-        
-        sunrise = datetime.utcfromtimestamp(int(data['sys']['sunrise'])).strftime('%H:%M:%S')
-        sunset = datetime.utcfromtimestamp(int(data['sys']['sunset'])).strftime('%H:%M:%S')
-
-        data_message = (
-            f"\nAt a temperature of {data['main']['temp']}c with lows of {round(data['main']['temp_min'])}c and highs of {round(data['main']['temp_max'])}c\n"
-            f"With {data['main']['humidity']}% humidity and {data['clouds']['all']}% cloud coverage\n"
-            f"Sunrise at {sunrise} and Sunset at {sunset}"
-            )
+        print(get_todays_weather())
+        input("Press Enter to continue!")
 
     if selection == 2:
-        pass
+        print(get_forecast())
+        input("Press Enter to continue!")
+
     if selection == 3:
         pass
     if selection == 4:
